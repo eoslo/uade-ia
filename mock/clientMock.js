ca961e06121240fd7f181f87456909b7825820d6
lluciano <lucio.luciano@mercadolibre.com>
2018-10-29 10:59:51 -0300

    mock

---
+++ mock/clientMock.js
@@ -0,0 +1,9 @@
+class clientMock{
+    constructor(name, creation_date, doc, status) {
+        this.name=name;
+        this.creation_date = creation_date;
+        this.doc=doc;
+        this.status=status
+    }
+}
+module.exports=clientMock;
