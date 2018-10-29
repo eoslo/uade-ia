60c8f924b092b855a5def2ba6e4778dd6ebadbe1
lautarofain <lautarofain@gmail.com>
2018-10-29 11:31:45 -0300

    Pushin mocks

--- mock/clientMock.js
+++ mock/clientMock.js
@@ -1,9 +1,14 @@
 class clientMock{
-    constructor(name, creation_date, doc, status) {
+    constructor(name, person_type, address, cuit, iva, gross_income, creation_date, doc, status) {
         this.name=name;
-        this.creation_date = creation_date;
+        this.person_type=person_type;
+        this.address=address;
+        this.cuit=cuit;
+        this.iva=iva;
+        this.gross_income=gross_income;
+        this.creation_date=creation_date;
         this.doc=doc;
-        this.status=status
+        this.status=status;
     }
 }
 module.exports=clientMock;
---
+++ mock/employeeMock.js
@@ -0,0 +1,12 @@
+class employeeMock{
+    constructor(name, address, birth_date, dni, creation_date, doc, status) {
+        this.name=name;
+        this.address=address;
+        this.birth_date=birth_date;
+        this.dni=dni;
+        this.creation_date=creation_date;
+        this.doc=doc;
+        this.status=status;
+    }
+}
+module.exports=employeeMock;
