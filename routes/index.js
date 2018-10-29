66e25de0d19b9c078045d52895c259a0170547fd
akhil-asaad <aquiles19939@gmail.com>
2018-10-29 12:42:42 -0300

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var passport = require('passport');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', !notLoggedIn, function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
@@ -11,11 +11,15 @@ router.get('/login', notLoggedIn, function (req, res, next) {
     res.render('login');
 });
 
+router.get('/', function (req, res, next) {
+    res.render('register');
+});
+
 router.post('/signup', notLoggedIn, passport.authenticate('local.signup', {
     successRedirect: '/',
     failureRedirect: '/signup',
     failureFlash: true
-}))
+}));
 
 function notLoggedIn(req, res, next) {
     if(!req.isAuthenticated()){
