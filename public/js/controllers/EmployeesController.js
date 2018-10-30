a209c6968a53072926655523b40e3a3c46ad0e06
akhil-asaad <aquiles19939@gmail.com>
2018-10-30 11:26:12 -0300

    update employees

--- public/js/controllers/EmployeesController.js
+++ public/js/controllers/EmployeesController.js
@@ -4,21 +4,34 @@ angular.module('PaychecksApp')
 
 .controller('EmployeesController', ['$scope', '$rootScope', '$http', '$window', function ($scope, $rootScope, $http, $window) {
     $scope.employees = [];
-
-    $scope.form = {
-        name: null,
-        address: null,
-        birth_day: null,
-        dni: null,
-        payroll_type: 'monthly',
-        gross_salary: null,
-        salary_per_hour: null,
-        client_id: null
-    }
+    initEmployeeForm();
 
     $scope.getUserId = function(userId) {
+        $scope.userId = userId;
         $scope.form.client_id = userId;
         getClientEmployees();
+    };
+
+    $scope.newEmployee = function() {
+        initEmployeeForm();
+    };
+
+    $scope.getEmployee = function(id) {
+        for (var i = 0; i < $scope.employees.length; i++) {
+            if($scope.employees[i]._id == id) {
+                $scope.form = {
+                    employee_id: id,
+                    name: $scope.employees[i].name,
+                    address: $scope.employees[i].address,
+                    birth_day: $scope.employees[i].birth_day,
+                    dni: $scope.employees[i].dni,
+                    payroll_type: $scope.employees[i].payroll_type,
+                    gross_salary: $scope.employees[i].gross_salary,
+                    salary_per_hour: $scope.employees[i].salary_per_hour,
+                    client_id: $scope.userId
+                }
+            }
+        }
     };
 
     $scope.registerEmployee = function() {
@@ -46,5 +59,18 @@ angular.module('PaychecksApp')
         .catch(function(error) {
             console.log(error);
         });
+    }
+
+    function initEmployeeForm() {
+        $scope.form = {
+            name: null,
+            address: null,
+            birth_day: null,
+            dni: null,
+            payroll_type: 'monthly',
+            gross_salary: null,
+            salary_per_hour: null,
+            client_id: null
+        }
     }
 }])
