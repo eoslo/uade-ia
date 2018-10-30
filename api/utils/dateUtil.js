10f41c6b79657db28398c25cda064bfee95c6bb7
lluciano <lucio.luciano@mercadolibre.com>
2018-10-30 18:00:13 -0300

    seconds

--- api/utils/dateUtil.js
+++ api/utils/dateUtil.js
@@ -5,11 +5,12 @@ class dateUtil{
         const year = String(d.getFullYear());
         let hours = String(d.getHours());
         let minutes = String(d.getMinutes());
+        let seconds = String(d.getSeconds());
 
         if (month.length < 2) month = '0' + month;
         if (day.length < 2) day = '0' + day;
 
-        return `${day}/${month}/${year} ${hours}:${minutes}`;
+        return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
     }
 
 }
