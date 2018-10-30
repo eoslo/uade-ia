578067802004f72289219b17414a46392d5d1f19
akhil-asaad <aquiles19939@gmail.com>
2018-10-30 08:53:12 -0300

    employees list

--- public/js/controllers/EmployeesController.js
+++ public/js/controllers/EmployeesController.js
@@ -3,7 +3,22 @@
 angular.module('PaychecksApp')
 
 .controller('EmployeesController', ['$scope', '$rootScope', '$http', '$window', function ($scope, $rootScope, $http, $window) {
+    $scope.employees = [];
     $scope.getUserId = function(userId) {
         $scope.userId = userId;
+        getClientEmployees();
+    }
+
+    function getClientEmployees() {
+        $http({
+            method: 'GET',
+            url: $rootScope.serverEndpoint + 'client/' + $scope.userId + '/employees'
+        })
+        .then(function(employees) {
+            $scope.employees = employees
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
     }
 }])
