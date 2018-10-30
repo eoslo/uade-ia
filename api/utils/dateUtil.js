b2fd3b33e1bc1def70b58e2d18c1a4283f731641
julianatlas <julianatlasovich@gmail.com>
2018-10-30 11:49:31 -0300

    added billing route and mock

---
+++ api/utils/dateUtil.js
@@ -0,0 +1,15 @@
+class dateUtil{
+    formattedDateArgentina(d = new Date) {
+        let month = String(d.getMonth() + 1);
+        let day = String(d.getDate());
+        const year = String(d.getFullYear());
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
+        return `${day}/${month}/${year}`;
+    }
+
+}
+
+module.exports=dateUtil;
