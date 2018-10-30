706cebb8877ba3636b8484bcba0252dcf615a57f
IgnacioPoire <ignaciopoire@gmail.com>
2018-10-30 09:20:28 -0300

    Mod Empleado Alta Formulario

--- public/js/controllers/EmployeesController.js
+++ public/js/controllers/EmployeesController.js
@@ -6,4 +6,27 @@ angular.module('PaychecksApp')
     $scope.getUserId = function(userId) {
         $scope.userId = userId;
     }
+    $scope.form = {
+        name: null,
+        address: null,
+        birth_day: null,
+        dni: null,
+        payroll_type: 'monthly',
+        gross_salary: null,
+        salary_per_hour: null,
+    }
+
+    $scope.registerEmployee = function() {
+        $http({
+            method: 'POST',
+            url: $rootScope.serverEndpoint + 'signup',
+            data: $scope.form
+        })
+            .then(function(response) {
+                $window.location.assign('/employee');
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    };
 }])
