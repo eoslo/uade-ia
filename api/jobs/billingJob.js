7a1dfb69af8450047f8dcfb75cb302200e08f305
lluciano <lucio.luciano@mercadolibre.com>
2018-10-31 10:13:17 -0300

    general fixes

--- api/jobs/billingJob.js
+++ api/jobs/billingJob.js
@@ -2,17 +2,12 @@ var schedule = require('node-schedule');
 var BillingService = require('../services/billingService');
 var billingService = new BillingService();
 
-/*
-var job = schedule.scheduleJob('* * * * *', function(){
+
+var job = schedule.scheduleJob('00 00 00 01 * *', function(){
     console.log("starting job scheduler")
-    billingService.createAllBills(function (err) {
-        if(err){
-            console.error(err);
-        }
-        console.log("finished job")
-    });
+    billingService.createAllBills();
 });
 
-*/
+
 
 module.exports = job;
