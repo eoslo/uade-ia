255cff8a3c15729701931d450ed3e8d478940272
lluciano <lucio.luciano@mercadolibre.com>
2018-10-31 10:50:18 -0300

    fix

--- api/jobs/billingJob.js
+++ api/jobs/billingJob.js
@@ -3,7 +3,7 @@ var BillingService = require('../services/billingService');
 var billingService = new BillingService();
 
 
-var job = schedule.scheduleJob('00 00 00 01 * *', function(){
+var job = schedule.scheduleJob('0 0 1 * *', function(){
     console.log("starting job scheduler")
     billingService.createAllBills();
 });
